f5910596ecf92855efa805d3b732cffeb4771741
usamahussaindev <usamahussaindev@gmail.com>
2023-10-23 03:58:06 +0500

    Final Commit

---
+++ lib/constants/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFE53935); // Red
+const Color tdBlue = Color(0xFF2196F3); // Blue
+const Color tdBlack = Color(0xFF717171);
+
+const Color tdGrey = Color(0xFF9E9E9E); // Grey
+const Color tdBGColor = Color(0xFFECEFF1); // Background Color
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:to_do_project/screens/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    //this is code to make statusbar more appealing
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ToDo App',
+      home: Home(),
+    );
+  }
+}
